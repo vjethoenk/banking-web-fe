059ea37d9867e2b83b0d9bfc516f27e58a4cb141
Viet Hoang <viethoang170504@gmail.com>
2026-08-12 08:48:10 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Banking Web
+      </h1>
+    </div>
+  )
+}
+
+export default App
+
+
